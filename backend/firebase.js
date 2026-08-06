798c94a2cbb20c8bd80ef1cf6d2b9e786be2c67e
zerubabelamtataw-sudo <zerubabelamtataw@gmail.com>
2026-08-06 19:06:53 +0300

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,10 +2,12 @@ const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+  });
+}
 
 const db = admin.database();
 

1a43f825e14d8bb1aa19dfe5aac6008d2b6b0cc4
zerubabelamtataw-sudo <zerubabelamtataw@gmail.com>
2026-08-22 14:04:15 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -872,7 +872,7 @@
     
       $('popupWinnerName').textContent = winner.playerName;
       $('popupCartelaDetail').textContent =
-  `Cartela #${winner.cartelaNumber}`;
+        `Cartela #${winner.cartelaNumber} · ${winner.calledCount} numbers called`;
        // Show winning cartela
     const winnerGrid = $('winnerCartelaGrid');
     
